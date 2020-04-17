d346acc56c628da0ae26d6fd382fdeb0d35e8b7a
abdel <abdel.douichi@gmail.com>
2020-04-17 15:05:21 +0200

    project skeleton

---
+++ src/main/java/com/offwait/offwait/OffwaitApplication.java
@@ -0,0 +1,13 @@
+package com.offwait.offwait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffwaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffwaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offwait/offwait/OffwaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offwait.offwait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffwaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
